83d44360b5217d8459515525371de82fc6ff1189
tejas <pangaonkar.svc424@gmail.com>
2025-06-20 13:03:34 +0530

    ai model change

--- configs/AiModel.tsx
+++ configs/AiModel.tsx
@@ -8,7 +8,8 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    // model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     // model: "gemini-2.5-flash-preview-04-17",
     // model: "gemini-2.5-pro-preview-03-25", // rpm is low in free 
   });
